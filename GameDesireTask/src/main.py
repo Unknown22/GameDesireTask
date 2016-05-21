fa7499b1271f4e5b415ecef172584804457b3fe6
Marcin Migda <marcin700@gmail.com>
2016-05-21 10:02:30 +0200

    Added method to get rankings by GAMEID

--- GameDesireTask/src/main.py
+++ GameDesireTask/src/main.py
@@ -1,6 +1,7 @@
 ﻿import tornado.ioloop
 import tornado.web
 import json
+from operator import itemgetter
 
 all_results = []
 
@@ -14,10 +15,7 @@ class AddResult(tornado.web.RequestHandler):
         end_timestamp = int(self.get_argument("end_timestamp"))
         game_result = self.get_argument("game_result")
 
-        self.check_gameid(gameid)
-        self.check_uid(uid)
-        self.check_end_timestamp(end_timestamp)
-        self.check_game_result(game_result)
+        self.check_arguments_correct(end_timestamp, game_result, gameid, uid)
 
         if self.correct_result:
             result = { 'GAMEID' : gameid,
@@ -30,12 +28,21 @@ class AddResult(tornado.web.RequestHandler):
         else:
             self.write("Result has not been added")
 
+
+    def check_arguments_correct(self, end_timestamp, game_result, gameid, uid):
+        self.check_gameid(gameid)
+        self.check_uid(uid)
+        self.check_end_timestamp(end_timestamp)
+        self.check_game_result(game_result)
+
+
     def check_gameid(self, gameid):
         if gameid != None:
             pass
         else:
             self.correct_result = False
             self.write("Invalid GAMEID<br>")
+
 
     def check_uid(self, uid):
         if self.represents_int(uid):
@@ -48,12 +55,14 @@ class AddResult(tornado.web.RequestHandler):
             self.correct_result = False
             self.write("Invalid UID<br>")     
 
+
     def check_end_timestamp(self, end_timestamp):
         if end_timestamp != None:
             pass
         else:
             self.correct_result = False
             self.write("Incorrect end timestamp<br>")      
+
 
     def check_game_result(self, game_result):
         if self.represents_float(game_result):
@@ -62,12 +71,14 @@ class AddResult(tornado.web.RequestHandler):
             self.write("Incorrect game result<br>")
             self.correct_result = False
         
+
     def represents_int(self, test_int):
         try:
             int(test_int)
             return True
         except ValueError:
             return False
+
 
     def represents_float(self, test_float):
         try:
@@ -77,9 +88,32 @@ class AddResult(tornado.web.RequestHandler):
             return False
 
 
+class ShowRanking(tornado.web.RequestHandler):
+
+    def get(self):
+        gameid = self.get_argument("gameid")
+        rank = self.get_ranking(gameid)
+
+
+    def get_ranking(self, gameid):
+        rank = []
+        
+        if gameid != None:
+            for result in all_results:
+                if json.loads(result)['GAMEID'] == gameid:
+                    rank.append(json.loads(result))
+        else:
+            self.write("Invalid GAMEID<br>")
+        
+        rank.sort(key=itemgetter('game_result'), reverse=True)
+
+        return rank
+
+
 if __name__ == "__main__":
     application = tornado.web.Application([
-        (r"/add", AddResult)
+        (r"/add", AddResult),
+        (r"/show_ranking", ShowRanking)
     ])
     application.listen(8888)
     tornado.ioloop.IOLoop.current().start()
